f8027fbe44653657ff531dc0239c7e4efb99a3e4
Carlos Mateus <carlosmatateumateus@gmail.com>
2025-02-04 21:42:59 +0100

    refactor

--- utils/logs.py
+++ utils/logs.py
@@ -1,11 +1,14 @@
 import logging
 from os import path
+
 import settings
 
-class CustomFormatter(logging.Formatter): 
-    def format(self, record): 
-        record.msg = str(record.msg)  
+
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        record.msg = str(record.msg)
         return super().format(record)
+
 
 log_format = "%(asctime)s: %(message)s"
 log_file = path.join(settings.logs_paths["root"], settings.logs_file)
@@ -13,7 +16,7 @@ log_file = path.join(settings.logs_paths["root"], settings.logs_file)
 middle_level = 35
 
 logging.basicConfig(
-    stream=open(log_file, 'a', encoding='utf-8'),
+    stream=open(log_file, "a", encoding="utf-8"),
     level=middle_level,
     format=log_format,
     datefmt=settings.logging_datefmt,
@@ -27,6 +30,7 @@ root_logger = logging.getLogger()
 root_logger.addHandler(console_handler)
 
 logger = logging.getLogger("app_logger")
+
 
 def log_message(message: str):
     logger.log(middle_level, message)
